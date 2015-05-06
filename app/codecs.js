ba66daa5789ce28674ded68b7fb85762e759e157
Krzysztof <krzysztof_mikuta@interia.pl>
2015-05-06 12:44:27 +0200

    JS corrects

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,37 @@
+/*jshint unused:false*/
 function codecA(a, b)
 {
+    'use strict';
+    /* jshint bitwise: false */
     if (a & b) {
         return a + b;
     } else {
         return a;
     }
 }
+
+/*jshint unused:false*/
 function codecB(a, b)
 {
-    if (a & b) {
+    'use strict';
+    if (a && b) {
         return a + b;
     } else {
         return a + ',' + b;
     }
 }
+
+/*jshint unused:false*/
 function codecC(a, b)
 {
+    'use strict';
+    /*jslint eqeq: true*/
     return a == b;
 }
+
+/*jshint unused:false*/
 function codecD(a, b)
 {
-    return a == b;
+    'use strict';
+    return a === b;
 }
