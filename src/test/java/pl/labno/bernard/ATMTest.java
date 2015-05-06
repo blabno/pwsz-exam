533ac2efbd4e7f9a228bc75ee09033787fbdbf7a
mikoxp <olesmichalxp@tlen.pl>
2015-05-06 11:05:38 +0200

    Test

--- src/test/java/pl/labno/bernard/ATMTest.java
+++ src/test/java/pl/labno/bernard/ATMTest.java
@@ -1,5 +1,58 @@
 package pl.labno.bernard;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
 public class ATMTest {
+
+    @Rule
+    public ExpectedException expectedException= ExpectedException.none();
+    @Test
+    public void withdrawCash_cardNoIsNull_Exception()
+    {
+     Bank bank=  Mockito.mock(Bank.class);
+        ATM atm=new ATM(bank);
+        atm.enterPin(1);
+        expectedException.expect(IllegalStateException.class);
+        atm.withdrawCash(0);
+    }
+    @Test
+    public void withdrawCash_pinIsNull_IllegalStateException()
+    {
+        Bank bank=  Mockito.mock(Bank.class);
+        ATM atm=new ATM(bank);
+        atm.enterCard(1);
+        expectedException.expect(IllegalStateException.class);
+        atm.withdrawCash(0);
+    }
+    @Test
+    public void withdrawCash_pinIsNotCorect_IllegalStateException()
+    {
+        Bank bank=  Mockito.mock(Bank.class);
+        when(bank.isPinCorrect(0,0)).thenReturn(false);
+        ATM atm=new ATM(bank);
+        atm.enterCard(0);
+        atm.enterPin(0);
+        expectedException.expect(IllegalStateException.class);
+        atm.withdrawCash(0);
+    }
+
+    @Test
+    public void withdrawCash_bankwithdrawThrow_IllegalStateException()
+    {
+        Bank bank=  Mockito.mock(Bank.class);
+        when(bank.isPinCorrect(0,0)).thenReturn(true);
+        when(bank.withdraw(0)).thenThrow(AccountBlockedException.class);
+        ATM atm=new ATM(bank);
+        atm.enterCard(0);
+        atm.enterPin(0);
+        expectedException.expect(IllegalStateException.class);
+        atm.withdrawCash(0);
+    }
 
 }
