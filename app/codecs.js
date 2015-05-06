3c20c4e88e0146d131b9d0bba023600e4394e822
zetbewu <olek52@onet.eu>
2015-05-06 10:55:53 +0200

    Update codecs.js

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,41 @@
-function codecA(a, b)
-{
-    if (a & b) {
-        return a + b;
-    } else {
-        return a;
+
+    function codecA(a, b)
+    {
+       'use strict';
+
+        if (a & b) {
+            return a + b;
+        } else 
+        {
+            return a;
+        }
+}
+
+
+    function codecB(a, b)
+    {
+        'use strict';
+
+        if (a & b) {
+            return a + b;
+        } else 
+        {
+            return a + ',' + b;
+        }
+}
+
+
+    function codecC(a, b)
+    {
+        return a == b;
+        'use strict';
+        return a === b;
     }
-}
-function codecB(a, b)
-{
-    if (a & b) {
-        return a + b;
-    } else {
-        return a + ',' + b;
-    }
-}
-function codecC(a, b)
-{
+
+
+    function codecD(a, b)
+    {
     return a == b;
-}
-function codecD(a, b)
-{
-    return a == b;
+    'use strict';
+    return a === b;
 }
