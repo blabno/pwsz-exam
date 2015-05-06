4a3cb7243741845fdd3a3cc3ddb5fcb89d1f78ad
ppodraza <podraza.piotr@gmail.com>
2015-05-06 10:38:30 +0200

    Changes in codecs

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,33 @@
 function codecA(a, b)
 {
-    if (a & b) {
-        return a + b;
-    } else {
-        return a;
-    }
+	if (a && b) {
+		return a + b;
+	} else {
+		return a;
+	}
 }
+
+
 function codecB(a, b)
 {
-    if (a & b) {
-        return a + b;
+	if (a && b) {
+		return a + b;
     } else {
-        return a + ',' + b;
-    }
+		return a + ',' + b;
+	}
 }
+
 function codecC(a, b)
 {
-    return a == b;
+	return a === b;
 }
+
 function codecD(a, b)
 {
-    return a == b;
+	return a === b;
 }
+
+codecA(1, 2);
+codecB(1, 2);
+codecC(1, 2);
+codecD(1, 2);
